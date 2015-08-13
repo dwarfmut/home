5fa12af2ce86261d926856f57c8e72683907109b
Daniel Faria <dwarfmut@gmail.com>
2015-08-13 17:17:59 -0300

    add docker alias

--- dot.bash.d/01-alias.sh
+++ dot.bash.d/01-alias.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-alias   ls="ls -G"
-alias grep="grep --color"
-alias   ll="ls -Glht"
+alias             ls="ls -G"
+alias           grep="grep --color"
+alias             ll="ls -Glht"
+alias  docker_puppet="docker run -itd --name puppetmaster -v ~/Gits/puppet-master/:/etc/puppet puppet"
+alias docker_varnish="docker run -id --name varnish --link puppetmaster varnish bash"
