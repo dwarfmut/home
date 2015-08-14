98035d03da4f5046a7f748fc755cea7bc4b3d0e6
Daniel Faria <daniel.faria@abril.com.br>
2015-08-14 19:10:29 -0300

    git diff

--- dot.bash.d/01-alias.sh
+++ dot.bash.d/01-alias.sh
@@ -3,6 +3,7 @@
 alias             ls="ls -G"
 alias           grep="grep --color"
 alias             ll="ls -Glht"
+alias           diff="git diff"
 alias      docker_rm="docker rm $(docker ps -lq)"
 alias  docker_puppet="docker run -itd --name puppetmaster -v ~/Gits/puppet-master/:/etc/puppet puppet"
 alias docker_varnish="docker run -it --name varnish --link puppetmaster varnish bash"
